c686c91e4d44df9e968c40b5c8ae84709e92cea8
doripjonov <doripjonov@exadel.com>
2021-04-07 02:22:30 +0300

    removed test functions

--- index.js
+++ index.js
@@ -14,32 +14,5 @@
  * permissions and limitations under the License.
  */
 import { CompreFace } from './core/compre_face.js';
-let server = "http://localhost";
-let port = 8000;
-let image_url = "https://media.gettyimages.com/photos/woman-standing-on-city-street-looking-at-camera-picture-id1292452761?s=2048x2048";
-let key = "00000000-0000-0000-0000-000000000002";
-let image_id = '00000000-0000-0000-0000-000000000022';
 
-let core = new CompreFace(server, port);
-let recognition_service = core.initFaceRecognitionService(key);
-let faceCollection = recognition_service.getFaceCollection();
-
-faceCollection.verify('../img/girl.png', image_id, {limit: 0, face_plugins: "age,gender"})
-    .then(res => console.log(res))
-    .catch(error => console.log(error))
-
-// recognition_service.recognize('../img/girl.png', {limit: 0, face_plugins: "age,gender", status: true })
-//     .then(res => console.log(JSON.stringify(res)))
-//     .catch(error => console.log(error))
-
-// faceCollection.list()
-//     .then(res => console.log(res))
-//     .catch(error => console.log(error))
-
-// faceCollection.add("../img/girl.png", "Cute girl")
-//     .then(res => console.log(res))
-//     .catch(error => console.log(error))
-
-// faceCollection.delete_all_subject("Cute girl")
-//     .then(res => console.log(res))
-//     .catch(error => console.log(error))
+export { CompreFace };
