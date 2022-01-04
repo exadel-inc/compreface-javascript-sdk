a7f3775f620063eba6c12975c284b977c4bedea3
vladp <de5pot162@gmail.com>
2022-01-04 22:25:21 +0200

    fixed issue

--- face_founder_demo/src/App.js
+++ face_founder_demo/src/App.js
@@ -5,7 +5,7 @@ import { Container, Row, Form, Col, Button, ProgressBar, Spinner } from 'react-b
 import UploadedImage from './components/UploadedImge';
 import DisplayFoundImages from './components/DisplayFoundImages';
 import DisplayCropedImage from './components/DisplayCropedImages';
-import { confirmAlert } from 'react-confirm-alert'; 
+import { confirmAlert } from 'react-confirm-alert';
 import {NotificationContainer, NotificationManager} from 'react-notifications';
 import ShowBadImages from './components/ShowBadImages';
 import 'react-confirm-alert/src/react-confirm-alert.css';
@@ -26,19 +26,25 @@ function App() {
   const [ detError, setDetError ] = useState([]);
   const [ showInvalidFiles, setVisibilityInvalidFiles ] = useState(false);
   const [ bigImage, setBigImages ] = useState([]);
-  const [ loading, setLoading ] = useState(false)
- 
+  const [ loading, setLoading ] = useState(false);
+
+  useEffect(() => {
+    if(imageFolder !== null) {
+      findImages();
+    }
+  }, [imageFolder]);
+
   const canvasElement = useRef(null);
   const imageElement = useRef(null);
 
   let server = "http://localhost";
-  let port = 8000; 
+  let port = 8000;
 
   let core = new CompreFace(server, port);
-  
+
   let recognitionService = core.initFaceRecognitionService(recognitionKey);
   let faceCollection = recognitionService.getFaceCollection();
-  let detectionService = core.initFaceDetectionService(detectionKey) 
+  let detectionService = core.initFaceDetectionService(detectionKey)
 
   const removeDublicates = (array) =>{
     let tempArray = [];
@@ -68,12 +74,12 @@ function App() {
       setCounter(0)
       setRecError(prevArray => [...new Set(prevArray)])
       setVisibilityInvalidFiles(true);
-    
+
       const reader = new FileReader();
       reader.onload = myEvent => {
         setUploadedImage( myEvent.target.result )
         let sendata = myEvent.target.result.split(',')[1];
-        
+
         recognitionService.recognize(sendata, { limit: 10, prediction_count: 100, face_plugins: "age,gender"})
           .then(res => {
             setLoading(false)
@@ -101,7 +107,7 @@ function App() {
   }
 
   const onRateChanges = e => {
-    let number = e.target.value; 
+    let number = e.target.value;
     setFoundFaces([]);
     setRate(number);
 
@@ -113,7 +119,7 @@ function App() {
       for(let i = 0; i < Object.values(imageFolder).length; i++ ){
         if(subject[0] === imageFolder[i]['name']) setFoundFaces(oldFaces => [...oldFaces, imageFolder[i]])
       }
-    }) 
+    })
   }
 
   const onChangeImageFolder = event => {
@@ -127,7 +133,7 @@ function App() {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.readAsDataURL(file);
-   
+
       reader.onload = () => resolve({ base64: reader.result, fileName: file.name });
       reader.onerror = error => reject(error);
     });
@@ -135,7 +141,7 @@ function App() {
 
   const drawFace = (ctx, aface, index, fileName, lengthOfDetectedFaces, callback, itaration) => {
     ctx.drawImage(imageElement.current, aface.box.x_min, aface.box.y_min, aface.box.x_max - aface.box.x_min, aface.box.y_max - aface.box.y_min, 0, 0, aface.box.x_max - aface.box.x_min, aface.box.y_max - aface.box.y_min);
-    
+
     canvasElement.current.toBlob( blob => {
       faceCollection.add(blob, fileName)
         .then(data => {
@@ -172,21 +178,21 @@ function App() {
     }, 'image/jpeg', 0.95)
   }
 
-  const findImages = e => {   
+  const findImages = e => {
     setDetError([]);
     setRecError([]);
     setVisibilityInvalidFiles(false);
 
     let internalCounter = 0;
 
-    const uploadRecursivley = (intarator) => { 
+    const uploadRecursivley = (intarator) => {
       // stop processing at the end of file
       if(!imageFolder.item(intarator)){
         NotificationManager.success("Compeleted!", "Image processing completed!", 3000)
         return null;
       }
-      
-      // check size of image 
+
+      // check size of image
       if(imageFolder.item(intarator).size < 5000000){
         // get base64 format of image
         getBase64(imageFolder.item(intarator))
@@ -194,15 +200,15 @@ function App() {
             setPersonsImage(customData.base64)
             let sendata = customData.base64.split(',')[1];
             let ctx = canvasElement.current.getContext('2d');
-            
+
             detectionService.detect(sendata)
               .then(res => {
                 res.result.forEach((aface, index) => {
                   drawFace(ctx, aface, index, customData.fileName, res.result.length, uploadRecursivley, intarator)
                 })
               })
-              .catch(error => {   
-                setDetError(prevArray => [...prevArray, customData.fileName]) 
+              .catch(error => {
+                setDetError(prevArray => [...prevArray, customData.fileName])
                 confirmAlert({
                   title: 'Detection error',
                   message: `Face not found in ${customData.fileName}`,
@@ -266,20 +272,15 @@ function App() {
             </Form.Group>
           </Col>
         </Row>
-        <Row className="marginTop-15">
-          <Col>
-            <Button className="btn btn-primary btn-md float-right" disabled = { (imageFolder && recognitionKey && detectionKey) ? false : true }  onClick={findImages}>Upload folder</Button>
-          </Col>
-        </Row>
       </Form>
       <Row>
         <Col md={3}>
           { uploadedImage && <UploadedImage uploadedImage = { uploadedImage } />}
           { showInvalidFiles && <div>
             { recError.length && <ShowBadImages data={recError} badTitle="Recognition error occured in" />}
-            { detError.length && <ShowBadImages data={detError} badTitle="Detection error occured in" />}  
-            { bigImage.length && <ShowBadImages data={bigImage} badTitle="Oversized images" />} 
-          </div> } 
+            { detError.length && <ShowBadImages data={detError} badTitle="Detection error occured in" />}
+            { bigImage.length && <ShowBadImages data={bigImage} badTitle="Oversized images" />}
+          </div> }
         </Col>
         <Col md={9}>
           { loading &&  <div style={{ margin: "20px auto" }} >
@@ -296,11 +297,11 @@ function App() {
            <ProgressBar animated now={ ((counter + 1)/Object.values(imageFolder).length) * 100 } />
           { counter === Object.values(imageFolder).length ? <div>
             <span>Done you can select persons image</span>
-          </div> : <div> <span>Processing images</span> </div>} 
+          </div> : <div> <span>Processing images</span> </div>}
         </Col>
-      </Row>} 
+      </Row>}
       <Row> { personsImage && <DisplayCropedImage personsImage = { personsImage } imageElement = { imageElement } canvasElement = { canvasElement } />} </Row>
-      <NotificationContainer/>    
+      <NotificationContainer/>
     </Container>
   );
 }
